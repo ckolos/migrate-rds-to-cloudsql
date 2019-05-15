d8e47f85b9a24cdd8561497f1837ced69c2904f6
Chris Kolosiwsky <ckolos@gmail.com>
2019-05-15 16:53:29 -0500

    formatting

--- scripts/start_replication.sh
+++ scripts/start_replication.sh
@@ -20,20 +20,55 @@ echo "FOUND BINLOG POSITION: $POS"
 # Get list of all databases
 DATABASES="${databases_space_sep}"
 if [[ -z "$DATABASES" ]]; then
-  DATABASES=$(mysql -h ${rds_snapshot_instance} -u ${rds_backup_username} --password=${rds_backup_password} -NBe 'show schemas' | grep -wv 'mysql\|information_schema\|performance_schema')
+  DATABASES=$(mysql -h ${rds_snapshot_instance} \
+    -u ${rds_backup_username} \
+    --password=${rds_backup_password} \
+    -NBe 'show schemas' | grep -wv 'mysql\|information_schema\|performance_schema')
 fi
 
 # Dump database
 echo "Copying databases to intermediate instance"
-mysqldump -h ${rds_snapshot_instance} -u ${rds_backup_username} --password=${rds_backup_password} --databases $DATABASES --order-by-primary --skip-add-drop-table --skip-add-locks --skip-disable-keys --skip-set-charset --no-autocommit --default-character-set=utf8 --single-transaction | mysql -u root --password=${intermediate_root_password}
+mysqldump \
+  -h ${rds_snapshot_instance} \
+  -u ${rds_backup_username} \
+  --password=${rds_backup_password} \
+  --databases $DATABASES \
+  --order-by-primary \
+  --skip-add-drop-table \
+  --skip-add-locks \
+  --skip-disable-keys \
+  --skip-set-charset \
+  --no-autocommit \
+  --default-character-set=utf8 \
+  --single-transaction | mysql -u root --password=${intermediate_root_password}
 
 # Set master status
 echo "Setting master status"
-mysql -u root --password=${intermediate_root_password} -e "CHANGE MASTER TO MASTER_HOST = '${rds_original_instance}', MASTER_PORT = 3306, MASTER_USER = '${rds_replication_username}', MASTER_PASSWORD = '${rds_replication_password}', MASTER_LOG_FILE = '$FILE', MASTER_LOG_POS = $POS;"
+mysql \
+  -u root \
+  --password=${intermediate_root_password} \
+  -e "CHANGE MASTER TO MASTER_HOST = '${rds_original_instance}', 
+  MASTER_PORT = 3306, 
+  MASTER_USER = '${rds_replication_username}', 
+  MASTER_PASSWORD = '${rds_replication_password}', 
+  MASTER_AUTO_POSITION=1;"
 
 # Dump to GCLOUD bucket (please note: we dump from the intermediate here because then we have GTIDs)
 echo "Backing up to GCLOUD bucket ${gcs_bucket}"
-mysqldump -u root --password=${intermediate_root_password} --databases $DATABASES --order-by-primary --skip-add-drop-table --skip-add-locks --skip-disable-keys --skip-set-charset --no-autocommit --default-character-set=utf8 --single-transaction --set-gtid-purged=on --master-data=1 | gzip | gsutil cp - gs://${gcs_bucket}/dump.sql.gz
+mysqldump \
+  -u root \
+  --password=${intermediate_root_password} \
+  --databases $DATABASES \
+  --order-by-primary \
+  --skip-add-drop-table \
+  --skip-add-locks \
+  --skip-disable-keys \
+  --skip-set-charset \
+  --no-autocommit \
+  --default-character-set=utf8mb4 \
+  --single-transaction \
+  --set-gtid-purged=on \
+  --master-data=1 | gzip | gsutil cp - gs://${gcs_bucket}/dump.sql.gz
 
 # Start replication
 echo "Starting replication to intermediate"
